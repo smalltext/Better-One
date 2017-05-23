759daac56e6d57f1850c630cd807e31aa5b72952
vyroj <leepong.amerman@gmail.com>
2017-05-23 14:16:28 -0400

    Add files via upload
    Changing the name because unity already has a class called charactercontroller.

---
+++ Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public Parameters _parameters;
+    public ControllerState _controllerstate;
+
+	void Awake () {
+		
+	}
+
+    public void SetHorizontalForce(float x)
+    {
+        
+    }
+
+    public void SetVerticalForce(float y)
+    {
+
+    }
+	
+}
