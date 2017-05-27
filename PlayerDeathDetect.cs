5312b9da1229cc8e934eb34751ff66fdd1820c96
kevkevs <awoufackkevin@outlook.com>
2017-05-27 16:57:32 -0400

    Update PlayerDeathDetect.cs

--- PlayerDeathDetect.cs
+++ PlayerDeathDetect.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeathDetect : MonoBehaviour {
+	
+	public int deathSpeed;
+	bool playerDead;
+	
 
 	// Use this for initialization
 	void Start () {
-		
+		playerDead = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.tag == "ground" && other.gameObject.GetComponent<Rigidbody2D>.velocity.y >= deathSpeed){
+			Destroy(other.gameObject);
+			playerDead = true;
+		}
 	}
 }
